80197a8f9cce9cb57a07897001156de1c739857d
Michael Wharmby <mtwharmby@gmail.com>
2020-11-23 00:51:12 +0100

    Fixes order of test
    This prevents creating an outfile.csv. Also adds todo.

--- tests/test_data_writer.py
+++ tests/test_data_writer.py
@@ -3,8 +3,7 @@ from thermocouple_logger.data_writer import DataWriter
 
 
 def test_write_data(monkeypatch, mocker):
-    dw = DataWriter("outfile.csv", 1606084449.7003403)
-
+    # TODO Move to fixture
     # Mock the open builtin. open returns open, so we set __enter__() to
     # return the mock. Set the write function to return something first!
     open_mock = mocker.MagicMock(name="open_mock")
@@ -16,6 +15,7 @@ def test_write_data(monkeypatch, mocker):
     fake_time = 1606084461.7003403
     monkeypatch.setattr("time.time", lambda: fake_time)
 
+    dw = DataWriter("outfile.csv", 1606084449.7003403)
     dw.write_data({"int_temp": 25.6, "tc_temp": 23.4})
     open_mock.assert_called_with("outfile.csv", "a")
     open_mock().__enter__().write.assert_called_with(
