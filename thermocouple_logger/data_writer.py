801939adbe3547e774b74a22836d8e87edd9be67
Michael Wharmby <mtwharmby@gmail.com>
2020-11-23 00:50:30 +0100

    Adds writing unix timestamp header to output file

--- thermocouple_logger/data_writer.py
+++ thermocouple_logger/data_writer.py
@@ -8,6 +8,11 @@ class DataWriter:
         self.filename = filename
         self.start_time = start_time
 
+        # TODO Make "w" and warn if exists
+        # TODO Needs test
+        with open(self.filename, "a") as out_f:
+            out_f.write(f"start_time={time.time()}{os.linesep}")
+
     def write_data(self, data):
         time_offs = time.time() - self.start_time
         with open(self.filename, "a") as out_f:
