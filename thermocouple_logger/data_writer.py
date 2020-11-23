454c20fbc56bb733212d9a6e2fbe7ccbbc7376ea
Michael Wharmby <mtwharmby@gmail.com>
2020-11-23 01:01:52 +0100

    Improves outfile header

--- thermocouple_logger/data_writer.py
+++ thermocouple_logger/data_writer.py
@@ -12,6 +12,10 @@ class DataWriter:
         # TODO Needs test
         with open(self.filename, "a") as out_f:
             out_f.write(f"start_time={time.time()}{os.linesep}")
+            out_f.write(
+                "Offset Time [s],Reference Temperature [°C],Thermocouple "
+                + f"Temperature [°C]{os.linesep}"
+            )
 
     def write_data(self, data):
         time_offs = time.time() - self.start_time
